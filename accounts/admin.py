220df23b1445477d11a1d1f69612f31fa2e4106b
Fractylz <ariffnorhishambusiness@gmail.com>
2025-03-25 01:40:09 +0800

    added customuser to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,13 @@ from .models import CustomUser, StudentProfile, SupervisorProfile
 
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "is_staff", "is_superuser"]
+    search_fields = ["username", "email"]
+    list_filter = ["is_staff", "is_superuser"]
+
+
 @admin.register(StudentProfile)
 class StudentProfileAdmin(admin.ModelAdmin):
     list_display = ["user", "student_id", "company", "session"]
