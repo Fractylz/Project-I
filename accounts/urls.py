c00e43954040cb514d13d932a7ce60a6d9b22018
Fractylz <ariffnorhishambusiness@gmail.com>
2025-03-25 03:49:25 +0800

    Refactor registration process to use class-based view and update templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import register
+from . import views
 
 urlpatterns = [
-    path("register/", register, name="register"),
+    path("register/", views.RegistrationFormView.as_view(), name="register"),
 ]
